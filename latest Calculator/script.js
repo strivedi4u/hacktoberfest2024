86602047d92e6e7b5de342e96c4b83dc9f5f11e7
Kartikjain2626 <kartikjain2626@gmail.com>
2024-10-02 14:08:21 +0530

    Latest Calculator

---
+++ latest Calculator/script.js
@@ -0,0 +1,16 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
