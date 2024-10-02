481e04bbbf8aac5b3f6b0dcf117ac21bd8b4099f
Shivang Trivedi <73780452+trivedi4u@users.noreply.github.com>
2024-10-02 10:51:08 +0530

    Create findLargestElement.py

---
+++ findLargestElement.py
@@ -0,0 +1,9 @@
+input_arr = [int(x) for x in input("Enter the element of Array (seperated by space): ").split()]
+print(input_arr)
+#using built-in function
+print(f"max element in the array is: {max(input_arr)}")
+#using loop
+for i in range(1,len(input_arr)):
+    if input_arr[0] < input_arr[i]:
+        input_arr[0] = input_arr[i]
+print(f"max element in the array is: {input_arr[0]}")
