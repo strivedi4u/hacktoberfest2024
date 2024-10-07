34c1480c85fc95178c0b042859e027f5abb51c6a
Mohit Panjikar <20210802090@dypiu.ac.in>
2024-10-07 09:33:46 +0530

    adding Valid paranthesis problem

---
+++ Valid_Paranthesis.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s) {
+        stack<char>st; 
+        for(auto it: s) {
+            if(it=='(' || it=='{' || it == '[') st.push(it); 
+            else {
+                if(st.size() == 0) return false; 
+                char ch = st.top(); 
+                st.pop(); 
+                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.empty(); 
+    }
+int main()
+{
+    string s="()[{}()]";
+    if(isValid(s))
+    cout<<"True"<<endl;
+    else
+    cout<<"False"<<endl;
+}
