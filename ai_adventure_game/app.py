e318d1a7fed677ea2832219b5a932588d38d0e52
Xanthium7 <akshhaykmurali@gmail.com>
2024-10-08 19:28:41 +0530

    set up cloufflre and caseendra db

--- ai_adventure_game/app.py
+++ ai_adventure_game/app.py
@@ -5,3 +5,17 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import json
+
+
+cloud_config = {
+    'secure_connect_bundle': 'secure-connect-choose-your-own-adventure.zip'
+}
+
+
+with open("choose_your_own_adventure-token.json") as f:
+    secrets = json.load(f)
+
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
+ASTRA_DB_KEYSPACE = "database"
+OPENAI_API_KEY = ""
