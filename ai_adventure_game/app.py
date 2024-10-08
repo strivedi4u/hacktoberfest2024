9f2ca6afeb36a260f4f556f8b5ee4965016c5ae1
Xanthium7 <akshhaykmurali@gmail.com>
2024-10-08 19:29:07 +0530

    set up message histpry buffer

--- ai_adventure_game/app.py
+++ ai_adventure_game/app.py
@@ -19,3 +19,23 @@ CLIENT_ID = secrets["clientId"]
 CLIENT_SECRET = secrets["secret"]
 ASTRA_DB_KEYSPACE = "database"
 OPENAI_API_KEY = ""
+
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+
+message_history = CassandraChatMessageHistory(
+    session_id="anything",
+    session=session,
+    keyspace=ASTRA_DB_KEYSPACE,
+    ttl_seconds=3600
+)
+
+message_history.clear()
+
+cass_buff_memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    chat_memory=message_history
+)
