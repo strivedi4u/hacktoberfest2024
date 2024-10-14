4a0dcb72b53829f13828dbab8adc936434979552
Arpit Kumar Pandey <119391563+arpit-2727@users.noreply.github.com>
2024-10-14 14:26:45 +0530

    Update Bubble_sort.cpp
    I Changed the basic function to Recursive function which makes it easier to understand and optimized way.

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -5,21 +5,18 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                // Swap arr[j] and arr[j + 1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+
+void sortArray(int *arr , int n){
+    if(n == 0 || n == 1){
+        return;
     }
+    for (int i = 0; i < n-1; i++) {
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+
+     }
+     sortArray(arr , n-1);
 }
 
 int main()
@@ -34,7 +31,7 @@ int main()
     }
     cout << endl;
 
-    bubbleSort(scores, n);
+   sortArray(scores, n);
 
     cout << "Sorted scores in ascending order: ";
     for (int i = 0; i < n; i++)
