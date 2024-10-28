f990015a21f1f178d55c60ded6c9da16156d6fc0
Mohammad Danish <83268762+Danish0703@users.noreply.github.com>
2024-10-28 10:36:18 -0700

    node js practice questions

---
+++ food/dabalkameetha.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name:"dabalkameetha",
+    style:"hyderabadi",
+};
---
+++ food/index.js
@@ -0,0 +1,5 @@
+const biryani=require("./biryani");
+const dabalkameetha=require("./dabalkameetha");
+const mojito=require("./mojito");
+let food = [biryani,dabalkameetha,mojito];
+module.exports=food;
